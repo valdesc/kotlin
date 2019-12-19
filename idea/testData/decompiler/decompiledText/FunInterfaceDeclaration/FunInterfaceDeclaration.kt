d151b5cf9decfdd40a28e34c3c1169a66f27d1b3
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-12-19 13:41:48 +0300

    FIC: support `fun` interfaces in stub builder

---
+++ idea/testData/decompiler/decompiledText/FunInterfaceDeclaration/FunInterfaceDeclaration.kt
@@ -0,0 +1,9 @@
+@Suppress("UNSUPPORTED_FEATURE")
+fun interface KRunnable {
+    fun invoke()
+}
+
+@Suppress("UNSUPPORTED_FEATURE")
+fun interface GenericKRunnable<T, R> {
+    fun invoke(t: T): R
+}
