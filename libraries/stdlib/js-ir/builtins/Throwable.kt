d51a22f87a8f9c8e5518e1747222773652b4deff
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-01-13 20:34:44 +0300

    ~ fix Throwable

--- libraries/stdlib/js-ir/builtins/Throwable.kt
+++ libraries/stdlib/js-ir/builtins/Throwable.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -21,7 +21,8 @@ public external open class Throwable {
     public constructor(cause: Throwable?)
     public constructor()
 
-    public override fun equals(other: Any?): Boolean
-    public override fun hashCode(): Int
+    // TODO: add specialized version to runtime
+//    public override fun equals(other: Any?): Boolean
+//    public override fun hashCode(): Int
     public override fun toString(): String
 }
